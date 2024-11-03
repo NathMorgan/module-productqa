df23f403b68d332caf0fcdcbc1abf767bc1299df
Nathan Morgan <nathan.morgan@tuqiri.net>
2024-11-03 21:32:52 +0000

    Add config for fetching notification email address

---
+++ Scope/Config.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bright\ProductQA\Scope;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class Config
+{
+    /** @var ScopeConfigInterface */
+    protected ScopeConfigInterface $scopeConfig;
+
+    protected const string XML_PATH_PRODUCTQA_ADMIN_NOTIFICATION_EMAIL = 'productqa/general/admin_notification_email';
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAdminNotificationEmail(): string
+    {
+        return $this->scopeConfig->getValue(self::XML_PATH_PRODUCTQA_ADMIN_NOTIFICATION_EMAIL) ?? '';
+    }
+}
